c0f0243fe4f844a56d5cd18738a0aa1df2037448
Alberto <amelchor675@yahoo.com>
2021-11-18 13:42:36 -0800

    added comments

--- Project_Phase2/data/animal_query_tables.sql
+++ Project_Phase2/data/animal_query_tables.sql
@@ -1,8 +1,12 @@
+
+-- We Want to select the animal breeds and the comments they receive when they have visits for those that were born before 2014
 SELECT animal_breed, visit_comment
 FROM animal,visits
 WHERE animal.animal_id = visits.animal_id
 AND strftime('%Y',animal.animal_dob) < '2014'
+GROUP BY animal_breed
 
+-- Select the city with the highest donations among the adoptee community
 SELECT city_name ,MAX(sums)
 FROM
 (SELECT city_name,SUM(money) as sums
@@ -12,11 +16,13 @@ AND shelter.city_key = city.city_key
 GROUP BY(city_name)
 )
 
+-- Select the names of the shelters that are in Ceres California
 SELECT shelter_name
 FROM shelter,city
 WHERE shelter.city_key = city.city_key
 AND shelter.city_key = 9
 
+-- Select the city and find the number of adoptees in the city
 SELECT city_name, COUNT(customer_id)
 FROM city, customer
 WHERE customer.city_key = city.city_key
