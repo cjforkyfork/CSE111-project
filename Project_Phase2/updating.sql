595e8b5978d1aed132fce43b519c23bf07eb55bb
Christian-John Forcadilla <cjforcadilla00@gmail.com>
2021-11-29 23:57:16 -0800

    updating tables, note the last three imports bad

---
+++ Project_Phase2/updating.sql
@@ -0,0 +1,21 @@
+.mode "csv"
+.separator ","
+
+-- .import '| tail -n +2 "/Users/christian-john/Downloads/CSE111/project/CSE111-project/Project_Phase2/data/animal.csv"' animal
+-- .import '| tail -n +2 "/Users/christian-john/Downloads/CSE111/project/CSE111-project/Project_Phase2/data/animals_assistant.csv"' animals_assistant
+
+-- .import '| tail -n +2 "/Users/christian-john/Downloads/CSE111/project/CSE111-project/Project_Phase2/data/city.csv"' city
+-- .import '| tail -n +2 "/Users/christian-john/Downloads/CSE111/project/CSE111-project/Project_Phase2/data/customer_community.csv"' customer_community
+-- .import '| tail -n +2 "/Users/christian-john/Downloads/CSE111/project/CSE111-project/Project_Phase2/data/customer.csv"' customer
+
+-- .import '| tail -n +2 "/Users/christian-john/Downloads/CSE111/project/CSE111-project/Project_Phase2/data/donations.csv"' donations
+-- .import '| tail -n +2 "/Users/christian-john/Downloads/CSE111/project/CSE111-project/Project_Phase2/data/requests.csv"' requests_visits
+-- .import '| tail -n +2 "/Users/christian-john/Downloads/CSE111/project/CSE111-project/Project_Phase2/data/shelter_assistant.csv"' shelter_assistant
+
+-- .import '| tail -n +2 "/Users/christian-john/Downloads/CSE111/project/CSE111-project/Project_Phase2/data/shelter.csv"' shelter
+-- .import '| tail -n +2 "/Users/christian-john/Downloads/CSE111/project/CSE111-project/Project_Phase2/data/status.csv"' status
+-- .import '| tail -n +2 "/Users/christian-john/Downloads/CSE111/project/CSE111-project/Project_Phase2/data/visits.csv"' visits
+
+
+-- Deletes
+-- DELETE FROM animals_assistant
