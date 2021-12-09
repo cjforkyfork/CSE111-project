2b20a085c0f3d6e95509360672eb898edfc71850
Alberto <amelchor675@yahoo.com>
2021-12-08 21:10:19 -0800

    revised schema and use case

--- Project_Phase2/animal_create_tables.sql
+++ Project_Phase2/animal_create_tables.sql
@@ -26,7 +26,7 @@ CREATE TABLE status (
     status_comment char(25) NOT NULL
 );
 CREATE TABLE animals_assistant (
-    assistant_id  INT NOT NULL,
+    assistant_id  varchar(55) NOT NULL,
     animal_id  INT NOT NULL,
     FOREIGN KEY(assistant_id) REFERENCES shelter_assistant(assistant_id),
     FOREIGN KEY(animal_id) REFERENCES animal(animal_id)
@@ -35,7 +35,7 @@ CREATE TABLE visits (
     visit_key   INT PRIMARY KEY NOT NULL,
     animal_id  INT  NOT NULL,
     status_key INT  NOT NULL,
-    assistant_id INT  NOT NULL,
+    assistant_id varchar(55)  NOT NULL,
     visit_comment char(100)  NOT NULL,
     FOREIGN KEY(animal_id) REFERENCES animal(animal_id),
     FOREIGN KEY(status_key) REFERENCES status(status_key),
@@ -43,27 +43,27 @@ CREATE TABLE visits (
 );
 CREATE TABLE donations (
     donation_key INT PRIMARY KEY NOT NULL,
-    customer_id  INT  NOT NULL,
+    customer_id  varchar(55)  NOT NULL,
     shelter_key  INT NOT NULL,
     money     decimal(9,2) NOT NULL,
     FOREIGN KEY(shelter_key) REFERENCES shelter(shelter_key)
 );
 CREATE TABLE shelter_assistant (
-    assistant_id     INT PRIMARY KEY NOT NULL,
+    assistant_id     varchar(55) PRIMARY KEY NOT NULL,
     assistant_name   varchar(55) NOT NULL,
     shelter_key     INT NOT NULL,
     FOREIGN KEY(shelter_key) REFERENCES shelter(shelter_key)
 );
 
 CREATE TABLE customer_community (
-    customer_id    INT NOT NULL,
+    customer_id    varchar(55) NOT NULL,
     shelter_key    INT NOT NULL,
     FOREIGN KEY(shelter_key) REFERENCES shelter(shelter_key)
     FOREIGN KEY(customer_id) REFERENCES customer(customer_id)
 );
 
 CREATE TABLE customer (
-    customer_id  INT PRIMARY KEY NOT NULL,
+    customer_id  varchar(55) PRIMARY KEY NOT NULL,
     customer_name char(25) NOT NULL,
     city_key INT NOT NULL,
     FOREIGN KEY(city_key) REFERENCES city(city_key)
@@ -71,7 +71,7 @@ CREATE TABLE customer (
 
 CREATE TABLE requests_visits (
     request_id INT NOT NULL,
-    customer_id    INT NOT NULL,
+    customer_id    varchar(55) NOT NULL,
     animal_id    INT NOT NULL,
     FOREIGN KEY(animal_id) REFERENCES shelter(animal_id)
     FOREIGN KEY(customer_id) REFERENCES customer(customer_id)
